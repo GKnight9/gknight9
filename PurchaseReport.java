8ec2fb0b1250ea0a4770a1a1d27045a871c5bd29
GKnight9 <63012852+GKnight9@users.noreply.github.com>
2020-04-01 21:24:49 -0600

    Add files via upload
    This is a coding practice used to keep track of purchases at a clothing store.

---
+++ PurchaseReport.java
@@ -0,0 +1,14 @@
+//Garrett Knight CSIS 1400 2/15/20
+public class PurchaseReport
+{
+   public static void main(String[] args)
+   {
+      System.out.print("=========================\nPurchase Report\n=========================\n");
+      System.out.printf("Shirt\t\t\t\t\t$%.2f\n", 21.99);
+      System.out.printf("Pants\t\t\t\t\t$%.2f\n", 49.97);
+      System.out.printf("Shoes\t\t\t\t\t$%.2f\n", 89.50);
+      System.out.printf("Belt\t\t\t\t\t$%.2f\n", 19.99);
+      System.out.printf("Coat\t\t\t\t  $%.2f\n", 129.95);
+      System.out.printf("=========================\nTotal\t\t\t\t  $%.2f", 311.40);
+   }
+}
